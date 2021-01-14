631c3b892e216e666aa81f1df81041dd14220b6f
Erik Johnson <r.erik.johnson@gmail.com>
2021-01-14 15:20:02 -0800

    Now creating an office 3D model when the Organization is loaded. - A new listener is listening for changes to the organization. - That listener triggers a Factory, which instantiates a prefab, based on a string/prefab mapping. - Office is in the wrong orientation and position.

---
+++ Assets/Code/Scriptable Variables/StringStringDictVariableListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Code.Scriptable_Variables {
+  
+  [Serializable]
+  public class StringStringUnityEvent : UnityEvent<string, string> {
+  }
+  
+  //A "listener" that picks up changes on a StringStringVariable and Invokes
+  //A Unity Event with the changed key/value pair.
+  public class StringStringDictVariableListener : MonoBehaviour {
+    [Tooltip("The StringStringVariable to listen to for changes.")]
+    [SerializeField] private StringStringVariable variable;
+
+    [Tooltip("The Unity Event triggered when the variable has changed.")]
+    public StringStringUnityEvent OnValueChangedEvent;
+    
+    private void OnEnable() {
+      variable.OnValueChanged += OnValueChanged;
+    }
+    private void OnDisable() {
+      variable.OnValueChanged -= OnValueChanged;
+    }
+
+    private void OnValueChanged(string  key, string value) {
+      OnValueChangedEvent?.Invoke(key, value);
+    }
+    
+  }
+}
