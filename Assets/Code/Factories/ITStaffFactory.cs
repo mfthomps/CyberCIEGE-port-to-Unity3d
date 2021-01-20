9bbe779450214b51a3e6cac6fdc5666db8cf5555
Erik Johnson <r.erik.johnson@gmail.com>
2021-01-20 13:20:13 -0800

    Refactored the ITStaffBehavior to use a ITStaffDataObject that is created from the ITStaffFactory.

--- Assets/Code/Factories/ITStaffFactory.cs
+++ Assets/Code/Factories/ITStaffFactory.cs
@@ -1,13 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace Code.Factories {
   //Factory that create ITStaff GameObjects
   public class ITStaffFactory : MonoBehaviour, iFactory {
+    public static Dictionary<string, ITStaffBehavior> staff_dict = new Dictionary<string, ITStaffBehavior>();
+    
     private static readonly string STAFF = "staff";
-    
+
     [SerializeField] private ITStaffBehavior _prefab;
-    
+
     //--------------------------------------------------------------------------
     public void Create(string filename, Transform parent = null) {
       throw new System.NotImplementedException();
@@ -17,16 +22,111 @@ namespace Code.Factories {
     public void CreateAll(string path, Transform parent = null) {
       LoadStaffFromFile(path, parent);
     }
-    
+
     //--------------------------------------------------------------------------
     private void LoadStaffFromFile(string path, Transform parent = null) {
       string user_dir = Path.Combine(path, STAFF);
       string[] clist = Directory.GetFiles(user_dir);
       foreach (string user_file in clist)
         if (user_file.EndsWith(".sdf")) {
-          ITStaffBehavior item = Instantiate(_prefab, parent);
-          item.LoadOneStaff(user_file);
+          ITStaffBehavior newStaff = Instantiate(_prefab, parent);
+          newStaff.Data = LoadOneStaff(user_file, newStaff);
+          UpdateGameObject(newStaff);
         }
+    }
+    
+    //--------------------------------------------------------------------------
+    private ITStaffDataObject LoadOneStaff(string user_file, ITStaffBehavior itStaffBehavior) {
+      var data = new ITStaffDataObject();
+      string cfile = Path.Combine(GameLoadBehavior.user_app_path, user_file);
+      
+      LoadStaff(cfile, itStaffBehavior, ref data);
+      
+      return data;
+    }
+    
+    //--------------------------------------------------------------------------
+    private static void LoadStaff(string filePath, ITStaffBehavior itStaffBehavior, ref ITStaffDataObject data) {
+      try {
+        StreamReader reader = new StreamReader(filePath, Encoding.Default);
+        using (reader) {
+          string tag;
+          ccUtils.PositionAfter(reader, "User");
+          string value;
+          do {
+            value = ccUtils.SDTNext(reader, out tag);
+            if (value == null || tag == null) {
+              continue;
+            }
+
+            switch (tag) {
+              case "Name":
+                data.user_name = value;
+                staff_dict.Add(data.user_name, itStaffBehavior);
+                break;
+              case "PosIndex":
+                if (!int.TryParse(value, out data.position)) {
+                  Debug.Log("Error: LoadStaff parsing position" + value);
+                }
+                break;
+              case "Dept":
+                data.department = value;
+                break;
+              case "Cost":
+                if (!int.TryParse(value, out data.cost)) {
+                  Debug.Log("Error: LoadStaff parsing cost" + value);
+                }
+                break;
+              case "Skill":
+                if (!int.TryParse(value, out data.skill)) {
+                  Debug.Log("Error: LoadStaff parsing skill" + value);
+                }
+
+                break;
+              case "HISupportSkill":
+                if (!int.TryParse(value, out data.hi_skill)) {
+                  Debug.Log("Error: LoadStaff parsing hi_skill" + value);
+                }
+
+                break;
+              case "HWSupportSkill":
+                if (!int.TryParse(value, out data.hw_skill)) {
+                  Debug.Log("Error: LoadStaff parsing hw_skill" + value);
+                }
+                break;
+            }
+          } while (value != null);
+        }
+      }
+      catch (Exception e) {
+        Debug.LogError(e.ToString());
+      }
+    }
+    
+    //--------------------------------------------------------------------------
+    private static void UpdateGameObject(ITStaffBehavior staff) {
+      staff.gameObject.SetActive(true);
+      staff.gameObject.name = $"ITStaff--{staff.Data.user_name}";
+      
+      int pos = staff.Data.position;
+      if (pos < 0) {
+        Debug.Log("LoadOneStaff got invalid pos for " + staff.Data.user_name);
+      }
+      else {
+        WorkSpace ws = WorkspaceFactory.GetWorkSpace(pos);
+        if (ws == null) {
+          Debug.Log("ITStaffBehavior got null workspace for pos" + pos);
+        }
+
+        if (!ws.AddUser(staff.Data.user_name)) {
+          Debug.Log("ITStaffBehavior AddUser, could not user, already populated " + staff.Data.user_name);
+        }
+
+        float xf, zf;
+        ccUtils.GridTo3dPos(ws.x, ws.y, out xf, out zf);
+        Vector3 v = new Vector3(xf - 1.0f, 0.5f, zf);
+        staff.transform.position = v;
+      }
     }
   }
 }
--- Assets/Code/ITStaffBehavior.cs
+++ Assets/Code/ITStaffBehavior.cs
@@ -1,141 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 using Code.Factories;
 using UnityEngine;
 
-public class ITStaffBehavior : MonoBehaviour {
-  private static Dictionary<string, ITStaffBehavior> staff_dict = new Dictionary<string, ITStaffBehavior>();
+namespace Code {
+  public class ITStaffBehavior : MonoBehaviour {
+    [SerializeField] private ITStaffDataObject _data;
   
-  private static Rect WindowRect = new Rect(10, 10, 250, 300);
-  public static Texture2D background, LOGO;
-  public string user_name;
-  public int position = -1;
-  public string department;
-  public string current_thought = "";
-  public int skill;
-  public int hw_skill;
-  public int hi_skill;
-  public int sw_skill;
-  public int cost;
-  public int salary;
+    private static Rect WindowRect = new Rect(10, 10, 250, 300);
 
-  private string filePath;
-  
-  public static void doItems() {
-    WindowRect = GUI.Window(1, WindowRect, HireMenu, "Hire IT/Security");
-  }
-
-  private static void HireMenu(int id) {
-    foreach (string key in staff_dict.Keys)
-      if (GUILayout.Button(key)) {
-        ITStaffBehavior script = staff_dict[key];
-        // TBD fix cost / salary to match game
-        XElement xml = new XElement("userEvent",
-          new XElement("hire",
-            new XElement("name", script.user_name),
-            new XElement("salary", script.cost)),
-          new XElement("cost", script.cost));
-
-        Debug.Log(xml);
-        IPCManagerScript.SendRequest(xml.ToString());
-        menus.clicked = "";
-      }
-
-    //Debug.Log("HireMenu clicked now " + menus.clicked);
-  }
-
-  public void LoadOneStaff(string user_file) {
-    
-    string cfile = Path.Combine(GameLoadBehavior.user_app_path, user_file);
-    
-    SetFilePath(cfile);
-    gameObject.SetActive(true);
-    LoadStaff();
-    int pos = position;
-    //Debug.Log("LoadUsers " + script.User_name + " pos is " + pos);
-    if (pos < 0) {
-      Debug.Log("LoadOneStaff got invalid pos for " + user_name);
-      return;
+    public ITStaffDataObject Data {
+      get => _data;
+      set => _data = value;
     }
 
-    if (pos >= 0) {
-      WorkSpace ws = WorkspaceFactory.GetWorkSpace(pos);
-      if (ws == null) {
-        Debug.Log("ITStaffBehavior got null workspace for pos" + pos);
-        return;
-      }
-
-      if (!ws.AddUser(user_name)) {
-        Debug.Log("ITStaffBehavior AddUser, could not user, already populated " + user_name);
-        return;
-      }
-
-      float xf, zf;
-      ccUtils.GridTo3dPos(ws.x, ws.y, out xf, out zf);
-      //Debug.Log(ws.x + " " + ws.y + " " + xf + " " + zf);
-      Vector3 v = new Vector3(xf - 1.0f, 0.5f, zf);
-      transform.position = v;
+    public static void doItems() {
+      WindowRect = GUI.Window(1, WindowRect, HireMenu, "Hire IT/Security");
     }
-    else {
-      Debug.Log("no postion for " + user_name);
+
+    private static void HireMenu(int id) {
+      foreach (string key in ITStaffFactory.staff_dict.Keys)
+        if (GUILayout.Button(key)) {
+          ITStaffBehavior script = ITStaffFactory.staff_dict[key];
+          // TBD fix cost / salary to match game
+          XElement xml = new XElement("userEvent",
+            new XElement("hire",
+              new XElement("name", script.Data.user_name),
+              new XElement("salary", script.Data.cost)),
+            new XElement("cost", script.Data.cost));
+
+          Debug.Log(xml);
+          IPCManagerScript.SendRequest(xml.ToString());
+          menus.clicked = "";
+        }
     }
-  }
-  
-  private void LoadStaff() {
-    try {
-      StreamReader reader = new StreamReader(filePath, Encoding.Default);
-      using (reader) {
-        string tag;
-        //Debug.Log("LoadUser read from " + filePath);
-        ccUtils.PositionAfter(reader, "User");
-        string value;
-        do {
-          value = ccUtils.SDTNext(reader, out tag);
-          if (value == null)
-            continue;
-          //Debug.Log("LoadUser got " + value + " for tag " + tag);
-          switch (tag) {
-            case "Name":
-              user_name = value;
-              //Debug.Log("LoadComponent adding to dict: " + this.user_name);
-              staff_dict.Add(user_name, this);
-              break;
-            case "PosIndex":
-              if (!int.TryParse(value, out position)) Debug.Log("Error: LoadStaff parsing position" + value);
-
-              break;
-            case "Dept":
-              department = value;
-              break;
-            case "Cost":
-              if (!int.TryParse(value, out cost)) Debug.Log("Error: LoadStaff parsing cost" + value);
-
-              break;
-            case "Skill":
-              if (!int.TryParse(value, out skill)) Debug.Log("Error: LoadStaff parsing skill" + value);
-
-              break;
-            case "HISupportSkill":
-              if (!int.TryParse(value, out hi_skill)) Debug.Log("Error: LoadStaff parsing hi_skill" + value);
-
-              break;
-            case "HWSupportSkill":
-              if (!int.TryParse(value, out hw_skill)) Debug.Log("Error: LoadStaff parsing hw_skill" + value);
-
-              break;
-          }
-        } while (value != null);
-      }
-    }
-    catch (Exception e) {
-      Console.WriteLine(e.Message + "\n");
-    }
-  }
-
-  public void SetFilePath(string path) {
-    filePath = path;
   }
 }
