0144a39acbd05013171354454fae358b3303b32a
Erik Johnson <r.erik.johnson@gmail.com>
2021-01-22 12:17:13 -0800

    Added convenience method to the ListVariable.

--- Assets/Code/Scriptable Variables/ListVariable.cs
+++ Assets/Code/Scriptable Variables/ListVariable.cs
@@ -4,6 +4,12 @@ using Shared.ScriptableVariables;
 namespace Code.Scriptable_Variables {
   //Base placeholder for ScriptableVariables that contain a list
   public class ListVariable<T> : ScriptableVariable<List<T>> {
-    
+
+    //Add an item to the list. Note, this creates a whole new List with the item added to it.
+    public void Add(T item) {
+      var thisList = Value;
+      thisList.Add(item);
+      Value = new List<T>(thisList);
+    }
   }
 }
