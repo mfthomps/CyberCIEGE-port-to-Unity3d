6e574ff955368cece5727c83181547c899ee3091
Eric Heine <erheine@nps.edu>
2021-01-25 11:21:45 -0800

    Fleshed out the core of the selected object panel system so an object can be selected and the correct type of property panel will display the object's properties.

---
+++ Assets/Code/User Interface/Main/Property Panel/BaseObjectPropertyPanel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Code.User_Interface.Main {
+  // We need a concrete base class for the property panels so we can use them in the Unity Inspector,
+  // but this class really doesn't do anything worthwhile other than that
+  public class BaseObjectPropertyPanel : MonoBehaviour {
+    // ------------------------------------------------------------------------
+    public virtual void DisplayProperties(GameObject gameObject) {}
+  }
+}
---
+++ Assets/Code/User Interface/Main/Property Panel/UserObjectPropertyPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Code.User_Interface.Main {
+  public class UserObjectPropertyPanel : GenericObjectPropertyPanel<UserBehavior> {
+    [Header("UI Elements")]
+    [Tooltip("Label for selected object's name")]
+    public TMP_Text nameLabel;
+
+    // ------------------------------------------------------------------------
+    protected override void DisplayProperties(UserBehavior component) {
+      nameLabel.text = component.Data.user_name;
+    }
+  }
+}
