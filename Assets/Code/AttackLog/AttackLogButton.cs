0b5bd572b09d04ecf50a1c512b5aabe484a2d9eb
Erik Johnson <r.erik.johnson@gmail.com>
2021-01-27 14:38:05 -0800

    Code styling

--- Assets/Code/AttackLog/AttackLogButton.cs
+++ Assets/Code/AttackLog/AttackLogButton.cs
@@ -3,24 +3,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 //used to manage the attack button
-public class AttackLogButton : MonoBehaviour {
-  [SerializeField]
-  private Button button;
+namespace Code.AttackLog {
+  public class AttackLogButton : MonoBehaviour {
+    [SerializeField]
+    private Button button;
 
-  [SerializeField]
-  private TMP_Text buttonText;
+    [SerializeField]
+    private TMP_Text buttonText;
   
-  public void SetHasNewLogs(bool newLogs) {
-    if (newLogs) {
-      if (buttonText) {
-        buttonText.fontStyle = FontStyles.Bold;
-        buttonText.faceColor = Color.red;
+    public void SetHasNewLogs(bool newLogs) {
+      if (newLogs) {
+        if (buttonText) {
+          buttonText.fontStyle = FontStyles.Bold;
+          buttonText.faceColor = Color.red;
+        }
       }
-    }
-    else {
-      if (buttonText) {
-        buttonText.fontStyle = FontStyles.Normal;
-        buttonText.faceColor = Color.black;
+      else {
+        if (buttonText) {
+          buttonText.fontStyle = FontStyles.Normal;
+          buttonText.faceColor = Color.black;
+        }
       }
     }
   }
