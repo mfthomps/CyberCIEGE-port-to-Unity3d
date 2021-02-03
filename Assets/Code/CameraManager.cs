0bc33be28291c2a3b4a944169c9c252ecd20cd99
Erik Johnson <r.erik.johnson@gmail.com>
2021-02-02 18:30:26 -0800

    Whipped up a circular list type to support the camera cycling through the Users, Computer, and Devices. - This replaces previous GetNext() code in a more generic way.

--- Assets/Code/CameraManager.cs
+++ Assets/Code/CameraManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UltimateCameraController.Cameras.Controllers;
 using Code.Scriptable_Variables;
@@ -11,10 +12,26 @@ namespace Code.Camera {
     public CameraController cameraController;
     [Tooltip("The Camera zoom we are managing")]
     public CameraZoom cameraZoom;
+    
     [Tooltip("List of users in the scenario")]
     [SerializeField] private UserListVariable _userList;
 
-    private UserBehavior _currentUserFocus;
+    [Tooltip("List of computers in the scenario")]
+    [SerializeField] private ComputerListVariable _computerList;
+    
+    [Tooltip("List of devices in the scenario")]
+    [SerializeField] private DeviceListVariable _deviceList;
+
+    private readonly CircularList<UserBehavior> _userCircList = new CircularList<UserBehavior>();
+    private readonly CircularList<ComputerBehavior> _computerCircList = new CircularList<ComputerBehavior>();
+    private readonly CircularList<DeviceBehavior> _deviceCircList = new CircularList<DeviceBehavior>();
+
+    // ------------------------------------------------------------------------
+    private void Awake() {
+      _userCircList.SetList(_userList.Value);
+      _computerCircList.SetList(_computerList.Value);
+      _deviceCircList.SetList(_deviceList.Value);
+    }
 
     // ------------------------------------------------------------------------
     public void PanCamera(Vector2 delta) {
@@ -33,20 +50,18 @@ namespace Code.Camera {
 
     // ------------------------------------------------------------------------
     public void MoveCameraToObject(WorldObjectType type) {
-      Transform newTargetObject = null;
+      MonoBehaviour newTargetObject = null;
       switch (type) {
         case WorldObjectType.Component:
           break;
         case WorldObjectType.Computer:
-          //newTargetObject = ComputerBehavior.GetNextComponent().transform;
-          //TODO
+          newTargetObject = _computerCircList.GetNext();
           break;
         case WorldObjectType.Device:
-          //newTargetObject = DeviceBehavior.GetNextComponent().transform;
-          //TODO
+          newTargetObject = _deviceCircList.GetNext();
           break;
         case WorldObjectType.User:
-          newTargetObject = GetNextUser().transform;
+          newTargetObject = _userCircList.GetNext();
           break;
         case WorldObjectType.Asset:
           break;
@@ -60,53 +75,25 @@ namespace Code.Camera {
           throw new ArgumentOutOfRangeException(nameof(type), type, null);
       }
 
-      if (newTargetObject) {
-        cameraController.targetObject = newTargetObject;
+      if (newTargetObject != null) {
+        cameraController.targetObject = newTargetObject.transform;
       }
     }
 
     // ------------------------------------------------------------------------
     public void OnScenarioStarted() {
       //TODO Where should the camera start from?
-      //This will auto slave the camera target to the first user. If none, try the first component.
-      UserBehavior ub = GetNextUser();
+      //This will auto slave the camera target to the first user. If none, try the first computer.
+      UserBehavior ub = _userCircList.GetNext();
       if (ub != null) {
         cameraController.targetObject = ub.transform;  
       }
       else {
-        //TODO
-        // var component = ComponentBehavior.GetNextComponent();
-        // if (component != null) {
-        //   cameraController.targetObject = component.transform;
-        // }
-      }
-    }
-
-    // ------------------------------------------------------------------------
-    private UserBehavior GetNextUser() {
-      UserBehavior first_user = null;
-      bool gotit = false;
-      foreach (var user in _userList.Value) {
-        if (!user.IsActiveUser())
-          continue;
-        if (gotit) {
-          _currentUserFocus = user;
-          return user;
+        var computer = _computerCircList.GetNext();
+        if (computer) {
+          cameraController.targetObject = computer.transform;
         }
-
-        if (first_user == null)
-          first_user = user;
-        if (_currentUserFocus == null) {
-          _currentUserFocus = user;
-          return user;
-        }
-
-        if (user == _currentUserFocus)
-          gotit = true;
       }
-
-      _currentUserFocus = first_user;
-      return first_user;
     }
   }
 }
---
+++ Assets/Code/CircularList.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Code {
+  //Utility class that wraps a List and provides methods to get the next/prev
+  //item in the list.
+  public class CircularList<T> where T : class {
+    private List<T> _list;
+    private int _currIndex = -1;
+
+    //-------------------------------------------------------------------------
+    public void SetList(List<T> items) {
+      _list = items;
+      _currIndex = _list.Count > 0 ? 0 : -1;
+    }
+    
+    //-------------------------------------------------------------------------
+    public T GetNext() {
+      Inc();
+      return _currIndex >= _list.Count ? null : _list[_currIndex];
+    }
+
+    //-------------------------------------------------------------------------
+    public T GetPrev() {
+      Dec();
+      return _currIndex > -1 ? _list[_currIndex] : null;
+    }
+
+    //-------------------------------------------------------------------------
+    private void Inc() {
+      _currIndex++;
+      if (_currIndex >= _list.Count) {
+        _currIndex = _list.Count > 0 ? 0 : -1;
+      }
+    }
+    
+    //-------------------------------------------------------------------------
+    private void Dec() {
+      _currIndex--;
+      if (_currIndex < 0) {
+        _currIndex = _list.Count - 1;
+      }
+    }
+
+  }
+}
