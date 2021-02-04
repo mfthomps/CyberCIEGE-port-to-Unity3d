b7c6aef9e71abb346d39a5fe7a9e0878b9d0de2e
Erik Johnson <r.erik.johnson@gmail.com>
2021-02-04 11:39:12 -0800

    Fixed an issue that would cause the game to try to remove a computer twice. - Now relying on the game server to send the "remove_computer" event, after sending the server a "sell" event.

--- Assets/Code/World Objects/Computer/ComputerManager.cs
+++ Assets/Code/World Objects/Computer/ComputerManager.cs
@@ -1,25 +1,22 @@
 ﻿using System.Xml.Linq;
-using Code.Factories;
 using UnityEngine;
 
 namespace Code.World_Objects.Computer {
   //Handles some functionality for ComputerBehaviors
   public class ComputerManager : MonoBehaviour {
-    [Tooltip("The ComputerFactory to use for adding/deleting computers.")]
-    [SerializeField] private ComputerFactory _computerFactory;
 
     //--------------------------------------------------------------------------
     public void OnScrapComputer(ComputerBehavior computer) {
-      //TODO Notify the game server this computer should be removed
+      //Notify the game server this computer should be removed
       SendScrapEvent(computer);
-      _computerFactory.Remove(computer);
+      
     }
 
     //--------------------------------------------------------------------------
     private static void SendScrapEvent(ComponentBehavior computer) {
       var xml = new XElement("componentEvent",
         new XElement("name", computer.Data.component_name),
-        new XElement("sell"));
+        new XElement("sell", ""));
 
       IPCManagerScript.SendRequest(xml.ToString());
     }
