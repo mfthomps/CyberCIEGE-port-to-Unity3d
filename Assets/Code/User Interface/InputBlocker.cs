c1813653d129dae83c309cb7e816f0038269de41
Eric Heine <erheine@nps.edu>
2021-02-04 09:18:05 -0800

    Attack Log and Scenario Details dialogs don't let UI inputs through so the camera can be rotated/zoomed when they're up.

---
+++ Assets/Code/User Interface/InputBlocker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Code.User_Interface {
+  // This class doesn't do anything except eat all UI types of input possible
+  public class InputBlocker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IScrollHandler {
+    // ------------------------------------------------------------------------
+    public void OnPointerEnter(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnPointerExit(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnPointerDown(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnPointerUp(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnPointerClick(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnBeginDrag(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnDrag(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnEndDrag(PointerEventData data) {
+    }
+
+    // ------------------------------------------------------------------------
+    public void OnScroll(PointerEventData data) {
+    }
+  }
+}
