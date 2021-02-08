b31bd6e616e2987c46f8cd8aad54098571fa221d
Erik Johnson <r.erik.johnson@gmail.com>
2021-02-08 12:58:51 -0800

    Refactored how the User avatar mesh gets assigned. - UserFactor now creates the appropriate avatar mesh instead of the UserBehavior - Added a male and female prefab variant - Added a scriptableVariable to map the gender to the avatar prefab.

---
+++ Assets/Code/Scriptable Variables/AvatarPrefabMapping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Code.World_Objects.User;
+using NaughtyAttributes;
+using Shared.ScriptableVariables;
+using UnityEngine;
+
+namespace Code.Scriptable_Variables {
+  [Serializable]
+  public class GenderToGameObject {
+    public UserGender _gender;
+    public GameObject _prefab;
+  }
+
+  //A ScriptableVariable that contains a list of User Genders to GameObjects.
+  //Used for creating new Users with the correct avatar mesh.
+  [CreateAssetMenu(menuName = "Scriptable Objects/Variables/CC/Avatar Mapping")]
+  public class AvatarPrefabMapping : ScriptableVariable {
+    [ReorderableList]
+    [SerializeField] private List<GenderToGameObject> _prefabs = new List<GenderToGameObject>();
+
+    //Get the first prefab that matches the supplied Gender. Null if one is not defined.
+    public GameObject GetPrefab(UserGender gender) {
+      var mapping = _prefabs.Find(x => x._gender == gender);
+      return mapping?._prefab;
+    }
+  }
+}
