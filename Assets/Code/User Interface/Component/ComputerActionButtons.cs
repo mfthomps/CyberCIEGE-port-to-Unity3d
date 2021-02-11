d8d8da51e8fadb748b5ca9a166394883b33088cf
Eric Heine <erheine@nps.edu>
2021-02-11 10:57:27 -0800

    Disabled action buttons on ComponentView when currently selected object isn't a computer.

---
+++ Assets/Code/User Interface/Component/ComputerActionButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Shared.ScriptableVariables;
+using Code.World_Objects.Computer;
+
+namespace Code.User_Interface.Components {
+  public class ComputerActionButtons : MonoBehaviour {
+    [Header("Input Variables")]
+    [Tooltip("Currently selected object")]
+    public GameObjectVariable selectedObject;
+
+    [Header("UI Elements")]
+    [Tooltip("List of Buttons to control based on the current selection")]
+    public List<Button> buttons;
+
+    // ------------------------------------------------------------------------
+    void OnEnable() {
+      selectedObject.OnValueChanged += UpdateButtonInteractability;
+      UpdateButtonInteractability();
+    }
+
+    // ------------------------------------------------------------------------
+    void OnDisable() {
+      selectedObject.OnValueChanged -= UpdateButtonInteractability;
+    }
+
+    // ------------------------------------------------------------------------
+    void UpdateButtonInteractability() {
+      var validSelection = selectedObject.Value != null && selectedObject.Value.GetComponent<ComputerBehavior>() != null;
+      foreach (var button in buttons) {
+        button.interactable = validSelection;
+      }
+    }
+  }
+}
