ed81e5c525cebb1a1867bd87798828f67b615035
Ryan <phfatmonkey@gmail.com>
2021-02-11 15:09:38 -0800

    prefbas for Office 6: double doors, single door; Office 5: Double doors, WIP single door

---
+++ Assets/Code/DoorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Animator animator;
+
+    private void OnTriggerEnter(Collider other) {
+      animator.SetBool("Open", true);
+    }
+
+    private void OnTriggerExit(Collider other) {
+      animator.SetBool("Open", false);
+
+    }
+}
