d458e652474352bf76d8af259be05304b8cda390
Ryan <phfatmonkey@gmail.com>
2021-02-16 11:11:30 -0800

    added mouse cursor images; added hover script to buttons

---
+++ Assets/Code/User Interface/Mouse/MouseHover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+  [SerializeField] private Texture2D hoverCursor;
+  private CursorMode cursorMode = CursorMode.Auto;
+  private Vector2 hotSpot = Vector2.zero;
+  
+  public void OnPointerEnter(PointerEventData pointerEventData)
+  {
+    Cursor.SetCursor(hoverCursor, hotSpot, cursorMode);
+  }
+
+  //Detect when Cursor leaves the ui element
+  public void OnPointerExit(PointerEventData pointerEventData)
+  {
+    Cursor.SetCursor(null, Vector2.zero, cursorMode);
+  }
+}
