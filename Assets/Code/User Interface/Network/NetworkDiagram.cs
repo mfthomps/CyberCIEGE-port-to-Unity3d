b0c12d94646f0a275992fe10db16ca8b6c950036
Ryan <phfatmonkey@gmail.com>
2021-02-17 11:40:44 -0800

    removed network connections

--- Assets/Code/User Interface/Network/NetworkDiagram.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NetworkDiagram : MonoBehaviour
-{
-  [SerializeField] private NetworkLink[] networkLinks;
- 
-}
