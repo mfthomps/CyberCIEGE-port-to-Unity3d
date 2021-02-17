295ae4f5a9c34f0afe89ad736d477f5359a1644a
Ryan <phfatmonkey@gmail.com>
2021-02-17 10:13:33 -0800

    WIP network links--although this may end up getting scrapped

---
+++ Assets/Code/User Interface/Network/NetworkDiagram.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkDiagram : MonoBehaviour
+{
+  [SerializeField] private NetworkLink[] networkLinks;
+ 
+}
---
+++ Assets/Code/User Interface/Network/NetworkLink.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkLink : MonoBehaviour
+{
+  private Image _image;
+  private Color _color;
+  private bool _visible;
+
+  // Start is called before the first frame update
+  private void Start()
+  {
+      _image = GetComponent<Image>();
+      if (!_image) {
+        Debug.Log("Network Link has no Image Component.");
+      }
+  }
+
+  // Color this link
+  public Color NetworkLinkColor {
+    get {
+      return _color;
+    }
+    set {
+      _color = value;
+      _image.color = _color;
+    }
+  }
+
+  // Make this link visible
+  public bool IsVisible {
+    get {
+      return _visible;
+    }
+    set {
+      _visible = value;
+      _image.enabled = _visible;
+    }
+  }
+
+
+}
