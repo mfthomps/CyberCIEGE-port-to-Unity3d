6ae9323fbe2c1348958ae9a85dfd0a5b5e0509ec
Erik Johnson <r.erik.johnson@gmail.com>
2021-02-17 14:32:39 -0800

    Quick fix to ensure the receptionist in the office5 is seated and typing.

---
+++ Assets/Code/StaticAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Code {
+  
+  //Utility to set a boolean animation parameter upon start up.
+  public class StaticAnimationController : MonoBehaviour {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private string _booleanAnimationParam;
+    [SerializeField] private bool _booleanAnimationParamValue;
+
+    //--------------------------------------------------------------------------
+    private void Start() {
+      if (!_animator) {
+        _animator.GetComponent<Animator>();
+      }
+      
+      if (_animator) {
+        _animator.SetBool(_booleanAnimationParam, _booleanAnimationParamValue);
+      }
+    }
+  }
+}
