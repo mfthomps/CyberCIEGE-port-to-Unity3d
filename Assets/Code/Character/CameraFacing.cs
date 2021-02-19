7d7bf2d956863dcdb71df50c4d4a92beac4b034a
Ryan <phfatmonkey@gmail.com>
2021-02-18 16:37:22 -0800

    added framework for thought bubble

---
+++ Assets/Code/Character/CameraFacing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacing : MonoBehaviour
+{
+  private Camera mainCamera;
+
+  [SerializeField] private Transform centerOfMass;
+  public float verticalOffset = 1f;
+  private void Start () {
+    mainCamera = Camera.main;
+  }
+  private void Update () {
+    Vector3 vector = mainCamera.transform.eulerAngles;
+    // Vector3 vector = new Vector3(mainCamera.transform.position.x - this.transform.position.x, mainCamera.transform.position.y - this.transform.position.y, mainCamera.transform.position.z - this.transform.position.z);
+    // vector.Normalize();
+    this.transform.eulerAngles = vector;
+    this.transform.localPosition = centerOfMass.localPosition + new Vector3(0f,verticalOffset,0f);
+
+  }
+}
---
+++ Assets/Code/Character/ThoughtBubble.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ThoughtBubble : MonoBehaviour
+{
+  
+  [SerializeField] private bool _Active;
+  [SerializeField] private string _startingThoughtBubbleText;
+  [SerializeField] private GameObject _thoughtBubbleRoot;
+
+  [SerializeField] private TMP_Text _thoughtBubbleTextComponent;
+  [SerializeField] private Image _thoughtBubbleSprite;
+  private string _thoughtBubbleText;
+  private bool _active;
+
+  private void Start () {
+    Active = _Active;
+    ThoughtBubbleText = _startingThoughtBubbleText;
+  }
+  private void Update () {
+    if (_startingThoughtBubbleText != ThoughtBubbleText)
+    {
+      ThoughtBubbleText = _startingThoughtBubbleText;
+    }
+    if (_Active != Active) {
+      Active = _Active;
+    }
+  }
+
+  public string ThoughtBubbleText {
+    get {
+      return _thoughtBubbleText;
+    }
+    set {
+      _thoughtBubbleText = value;
+
+      _thoughtBubbleTextComponent.text = _thoughtBubbleText;
+    }
+  }
+
+  public bool Active {
+    get {
+      return _active;
+    }
+    set {
+      _active = value;
+      _thoughtBubbleRoot.SetActive(_active);
+
+    }
+  }
+}
