d77e9518f499f0339643d24a577031ea900a163b
Erik Johnson <r.erik.johnson@gmail.com>
2021-02-22 11:41:17 -0800

    Mapped out some of the offsite offices

--- Assets/Code/Factories/OfficeModelFactory.cs
+++ Assets/Code/Factories/OfficeModelFactory.cs
@@ -1,26 +1,33 @@
-﻿using Code.Scriptable_Variables;
+﻿using System.Collections.Generic;
+using Code.Scriptable_Variables;
 using UnityEngine;
 
 namespace Code.Factories {
   //This class is responsible for creating and placing the 3D model of the office
   public class OfficeModelFactory : MonoBehaviour {
-    [Tooltip("The property name, defined in the Organization's properties, which " +
-             "maps to the office 3D model.")]
-    [SerializeField] private string _officePropertyName = "MainOfficeVersion";
+    [Tooltip("The property names, defined in the Organization's properties, which " +
+             "map to main office and remote office 3D models.")]
+    [SerializeField] private List<string> _officePropertyNames = new List<string>();
     
     [Tooltip("The ScriptableVariable that maps magic strings to actual 3D model prefabs.")]
     [SerializeField] private StringToPrefabMappingObject _prefabMapping;
-    
+
+    //-------------------------------------------------------------------------
     //This is called when the properties of an Organization have changed. If the correct
     //property has changed, try to find a prefab that corresponds and instantiate it.
     public void OrganizationPropertiesChanged(string key, string value) {
-      if (key == _officePropertyName) {
+      if (_officePropertyNames.Contains(key)) {
         CreateModel(value);
       }
     }
 
     //-------------------------------------------------------------------------
     private void CreateModel(string prefabKey) {
+      //ignore any values that aren't defined.
+      if (prefabKey == "none" || string.IsNullOrEmpty(prefabKey)) {
+        return;
+        
+      }
       var prefab = _prefabMapping.GetPrefabByKey(prefabKey);
       if (prefab) {
         Instantiate(prefab);
