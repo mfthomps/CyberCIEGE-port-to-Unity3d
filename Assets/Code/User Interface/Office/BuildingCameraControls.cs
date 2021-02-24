f3388da3fad4bfa4b1d4437d8624fce27e78ae4b
Eric Heine <erheine@nps.edu>
2021-02-24 09:12:34 -0800

    The building camera buttons hide when there's only one building in a scenario.

---
+++ Assets/Code/User Interface/Office/BuildingCameraControls.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Code.Scriptable_Variables;
+
+namespace Code.User_Interface.Office {
+  public class BuildingCameraControls : MonoBehaviour {
+    [Header("Input Variables")]
+    [Tooltip("List of ViewPoints that represent the different buildings available.")]
+    [SerializeField] private ViewPointListVariable _buildings;
+
+    [Header("UI Elements")]
+    [Tooltip("The root of the controls UI.")]
+    [SerializeField] private GameObject _controlsRoot;
+
+    // ------------------------------------------------------------------------
+    void OnEnable() {
+      _buildings.OnValueChanged += UpdateControlVisibility;
+      UpdateControlVisibility();
+    }
+
+    // ------------------------------------------------------------------------
+    void OnDisable() {
+      _buildings.OnValueChanged -= UpdateControlVisibility;
+    }
+
+    // ------------------------------------------------------------------------
+    private void UpdateControlVisibility() {
+      _controlsRoot.SetActive(_buildings.Value.Count > 1);
+    }
+  }
+}
