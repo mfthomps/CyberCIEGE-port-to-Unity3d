db9ff2d004eb6f9ab8277a13eff69d099331aa38
Erik Johnson <r.erik.johnson@gmail.com>
2021-02-24 17:25:30 -0800

    Roughed out the AI logic for the Staff IT guy to path to another User.

---
+++ Assets/Code/World Objects/User/AI/Conditions/ShouldVisitObject.cs
@@ -0,0 +1,12 @@
+﻿using Code.World_Objects.Character;
+using UnityEngine;
+
+namespace Code.World_Objects.User.AI.Conditions {
+  public class ShouldVisitObject : FsmCondition {
+    [SerializeField] private BaseCharacter _character;
+    
+    public override bool IsSatisfied(FsmState curr, FsmState next) {
+      return !string.IsNullOrEmpty(_character.GetData().Visiting);
+    }
+  }
+}
---
+++ Assets/Code/World Objects/User/AI/States/VisitObjectState.cs
@@ -0,0 +1,35 @@
+﻿using Code.Scriptable_Variables;
+using Code.World_Objects.Character;
+using UnityEngine;
+
+namespace Code.World_Objects.User.AI.States {
+  public class VisitObjectState : FsmState {
+    [SerializeField] private BaseCharacter _character;
+    [SerializeField] private UserListVariable _users;
+    [SerializeField] private ComputerListVariable _computers;
+
+    //--------------------------------------------------------------------------
+    public override void OnStateEnter() {
+      var targetStr = _character.GetData().Visiting;
+      if (string.IsNullOrEmpty(targetStr)) return;
+
+      //Get which GameObject this is
+      _character.CurrentNavTarget = GetTarget(targetStr);
+    }
+
+    //--------------------------------------------------------------------------
+    private GameObject GetTarget(string targetStr) {
+      var user = _users.Value.Find(x => x.Data.user_name == targetStr);
+      if (user) {
+        return user.gameObject;
+      }
+
+      var computer = _computers.Value.Find(x => x.Data.component_name == targetStr);
+      if (computer) {
+        return computer.gameObject;
+      }
+
+      return null;
+    }
+  }
+}
