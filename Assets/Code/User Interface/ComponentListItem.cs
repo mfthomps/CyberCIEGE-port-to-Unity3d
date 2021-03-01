c57d4a11f12d49d1b89601815be6f0a88e83ed50
Ryan <phfatmonkey@gmail.com>
2021-03-01 13:55:24 -0800

    this isn't working

--- Assets/Code/User Interface/ComponentListItem.cs
+++ Assets/Code/User Interface/ComponentListItem.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace Code.User_Interface {
@@ -7,11 +8,22 @@ namespace Code.User_Interface {
     [Tooltip("The element that should display the item's label string.")]
     [SerializeField]
     private TMP_Text labelUI;
+    
+    [Tooltip("The button that controls clicking on this network to toggle its connection state")]
+    public Button clickButton;
 
     //-------------------------------------------------------------------------
     public override void SetItem(ComponentBehavior item) {
       labelUI.text = item.Data.component_name;
       this.name = item.Data.component_name;
+    }
+
+    public void Click () {
+      base.Click();
+
+      SetSelected(true);
+
+      Debug.Log("Click");
     }
   }
 }
