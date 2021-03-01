72955b926e14ac1b88d29c02f9da4ee5c11b11d4
Erik Johnson <r.erik.johnson@gmail.com>
2021-03-01 15:36:31 -0800

    Null check

--- Assets/Code/World Objects/User/AI/States/NavigateState.cs
+++ Assets/Code/World Objects/User/AI/States/NavigateState.cs
@@ -7,6 +7,7 @@ namespace Code.World_Objects.User.AI.States {
     [Tooltip("Some fudge to compare current navigation target to the current destination." +
              "Used to decide if a new destination should be set.")]
     [SerializeField] private float _distanceTolerance = 1.25f;
+
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Animator _animator;
     [SerializeField] private Navigator _user;
@@ -20,7 +21,7 @@ namespace Code.World_Objects.User.AI.States {
 
       MaybeUpdateAgentDestination();
     }
-    
+
     //--------------------------------------------------------------------------
     public override void OnStateLeave() {
       _animator.SetBool(_walkingAnimParam, false);
@@ -30,9 +31,8 @@ namespace Code.World_Objects.User.AI.States {
     private void Update() {
       //while running, check if the current navigation target has changed, based on the 
       //last set NavMeshAgent's destination. If so, give the NavMeshAgent the new position.
-      
-      if (_user.CurrentNavTarget && _agent.isOnNavMesh ) {
 
+      if (_user.CurrentNavTarget && _agent.isOnNavMesh) {
         if (_agent.pathPending) {
           return;
         }
@@ -40,15 +40,19 @@ namespace Code.World_Objects.User.AI.States {
         if (_agent.pathStatus == NavMeshPathStatus.PathInvalid || _agent.pathStatus == NavMeshPathStatus.PathPartial) {
           Debug.Log("Invalid path");
         }
-        
+
         MaybeUpdateAgentDestination();
       }
     }
-    
+
     //--------------------------------------------------------------------------
     private void MaybeUpdateAgentDestination() {
       //Check if the agent's current destination is "different" than the navigation target.
       //This could happen if the navigation target changes.
+      if (!_user.CurrentNavTarget) {
+        return;
+      }
+
       var dist = Vector3.Distance(_user.CurrentNavTarget.transform.position, _agent.destination);
       if (dist > _distanceTolerance) {
         if (_agent.SetDestination(_user.CurrentNavTarget.transform.position)) {
