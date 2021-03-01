f7944168dcd91d634992948a97648c77ea4bbfc8
Erik Johnson <r.erik.johnson@gmail.com>
2021-03-01 15:23:16 -0800

    Updating the navMeshAgent's destination should the character's navigation target change enough.

--- Assets/Code/World Objects/User/AI/States/NavigateState.cs
+++ Assets/Code/World Objects/User/AI/States/NavigateState.cs
@@ -4,6 +4,9 @@ using UnityEngine.AI;
 namespace Code.World_Objects.User.AI.States {
   //A FSM state that gives a NavMeshAgent a path to the User's current nav target
   public class NavigateState : FsmState {
+    [Tooltip("Some fudge to compare current navigation target to the current destination." +
+             "Used to decide if a new destination should be set.")]
+    [SerializeField] private float _distanceTolerance = 1.25f;
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Animator _animator;
     [SerializeField] private Navigator _user;
@@ -15,15 +18,7 @@ namespace Code.World_Objects.User.AI.States {
         _agent.enabled = true;
       }
 
-      if (_user.CurrentNavTarget && _agent.isOnNavMesh) {
-        if (_agent.SetDestination(_user.CurrentNavTarget.transform.position)) {
-          _animator.SetBool(_walkingAnimParam, true);
-          Debug.Log($"Pathing [{_agent.name}] -> [{_user.CurrentNavTarget}]");
-        }
-        else {
-          Debug.LogError($"Can't path [{_user.name}] to [{_user.CurrentNavTarget.transform.position}]");
-        }
-      }
+      MaybeUpdateAgentDestination();
     }
     
     //--------------------------------------------------------------------------
@@ -35,18 +30,33 @@ namespace Code.World_Objects.User.AI.States {
     private void Update() {
       //while running, check if the current navigation target has changed, based on the 
       //last set NavMeshAgent's destination. If so, give the NavMeshAgent the new position.
-      const float fudge = 1.5f;
       
       if (_user.CurrentNavTarget && _agent.isOnNavMesh ) {
-        var dist = Vector3.Distance(_user.CurrentNavTarget.transform.position, _agent.destination);
-        if (dist > fudge) {
-          if (_agent.SetDestination(_user.CurrentNavTarget.transform.position)) {
-            _animator.SetBool(_walkingAnimParam, true);
-            Debug.Log($"Pathing [{_agent.name}] -> [{_user.CurrentNavTarget}]");
-          }
-          else {
-            Debug.LogError($"Can't path [{_user.name}] to [{_user.CurrentNavTarget.transform.position}]");
-          }
+
+        if (_agent.pathPending) {
+          return;
+        }
+
+        if (_agent.pathStatus == NavMeshPathStatus.PathInvalid || _agent.pathStatus == NavMeshPathStatus.PathPartial) {
+          Debug.Log("Invalid path");
+        }
+        
+        MaybeUpdateAgentDestination();
+      }
+    }
+    
+    //--------------------------------------------------------------------------
+    private void MaybeUpdateAgentDestination() {
+      //Check if the agent's current destination is "different" than the navigation target.
+      //This could happen if the navigation target changes.
+      var dist = Vector3.Distance(_user.CurrentNavTarget.transform.position, _agent.destination);
+      if (dist > _distanceTolerance) {
+        if (_agent.SetDestination(_user.CurrentNavTarget.transform.position)) {
+          _animator.SetBool(_walkingAnimParam, true);
+          Debug.Log($"Pathing [{_agent.name}] -> [{_user.CurrentNavTarget}]");
+        }
+        else {
+          Debug.LogError($"Can't path [{_user.name}] to [{_user.CurrentNavTarget.transform.position}]");
         }
       }
     }
