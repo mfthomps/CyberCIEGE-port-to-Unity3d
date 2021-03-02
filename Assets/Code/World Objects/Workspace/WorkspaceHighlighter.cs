3266f689cae739a1312d430a5318f6236ad157c4
Ryan <phfatmonkey@gmail.com>
2021-03-02 14:25:52 -0800

    added highlight mesh and textures

--- Assets/Code/World Objects/Workspace/WorkspaceHighlighter.cs
+++ Assets/Code/World Objects/Workspace/WorkspaceHighlighter.cs
@@ -5,14 +5,33 @@ namespace Code.World_Objects.Workspace {
     [Tooltip("The renderer of the highlighter")]
     public Renderer highlightRenderer;
 
+    [Tooltip("Color of the highlight when the workspace is available.")]
+    [SerializeField] private Color _availableColor = new Color(1.0f, 1.0f, 0.0f, 0.5f);
+
+    [Tooltip("Texture of the highlight when the workspace is available.")]
+    [SerializeField] private Texture2D _availableTexture;
+
+    [Tooltip("Color of the highlight when the workspace is unavailable.")]
+    [SerializeField] private Color _unavailableColor = new Color(1.0f, 0.0f, 0.0f, 0.5f);
+
+    [Tooltip("Texture of the highlight when the workspace is unavailable.")]
+    [SerializeField] private Texture2D _unavailableTexture;
+
     // ------------------------------------------------------------------------
     public void SetAvailable() {
-      highlightRenderer.material.color = new Color(1.0f, 1.0f, 0.0f, 0.5f);
+      highlightRenderer.material.color = _availableColor;
+      if (_availableTexture) {
+        highlightRenderer.material.mainTexture = _availableTexture;
+      }
+
     }
 
     // ------------------------------------------------------------------------
     public void SetUnavailable() {
-      highlightRenderer.material.color = new Color(1.0f, 0.0f, 0.0f, 0.5f);
+      highlightRenderer.material.color = _unavailableColor;
+      if (_unavailableTexture) {
+        highlightRenderer.material.mainTexture = _unavailableTexture;
+      }
     }
 
     // ------------------------------------------------------------------------
