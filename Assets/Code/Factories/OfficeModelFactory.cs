ab56dd4fec538a34379c03c099c47435768c8614
Erik Johnson <r.erik.johnson@gmail.com>
2021-03-03 17:04:47 -0800

    Setting the OfficeBuilding type based on how they are defined in the scenario. - initialized all building prefabs to "unknown" since they can change at runtime.

--- Assets/Code/Factories/OfficeModelFactory.cs
+++ Assets/Code/Factories/OfficeModelFactory.cs
@@ -60,6 +60,9 @@ namespace Code.Factories {
         go.transform.Translate(0, 0, offset, Space.World);
 
         OfficeBuilding building = go.GetComponent<OfficeBuilding>();
+        //the first in the list is considered the main office, he rest
+        //are offsites.
+        building.BuildingType = listIndex == 0 ? OfficeBuildingType.MainOffice : OfficeBuildingType.OffsiteOffice;
         _officeList.Add(building);
       }
       else {
