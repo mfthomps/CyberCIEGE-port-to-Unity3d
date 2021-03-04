8106c2cf7641128d44ca2319c37d513d33a49d61
Eric Heine <erheine@nps.edu>
2021-03-04 09:20:35 -0800

    Users' highest background check level gets updated when a clearance or group's background check level changes.

--- Assets/Code/Clearances/ClearanceBehavior.cs
+++ Assets/Code/Clearances/ClearanceBehavior.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using Code.Scriptable_Variables;
 
 namespace Code.Clearance {
   public class ClearanceBehavior : MonoBehaviour {
+    [Header("Input Variables")]
+    [Tooltip("The users in the current scenario.")]
+    [SerializeField] private UserListVariable users;
+
     [SerializeField] private ClearanceDataObject _data;
 
     public ClearanceDataObject Data {
@@ -12,6 +17,11 @@ namespace Code.Clearance {
     // ------------------------------------------------------------------------
     public void SetBackgroundCheckLevel(BackgroundCheck.Level level) {
       _data.backgroundCheckLevel = level;
+
+      // Update all the user's highest background check level
+      foreach (var user in users.Value) {
+        user.UpdateHighestBackgroundCheck();
+      }
     }
   }
 }
