fd567077edf1d4e65fcd09fed3bf3fd158f013a7
Eric Heine <erheine@nps.edu>
2021-03-04 10:39:30 -0800

    Made sure headers' text doesn't resize or wrap and instead truncates with an ellipsis and a tooltip (in case the screen size is too smalll) to give a more consistent appearance for different screen sizes.

--- Assets/Code/User Interface/Zone/ZoneSettingTypeListItem.cs
+++ Assets/Code/User Interface/Zone/ZoneSettingTypeListItem.cs
@@ -2,18 +2,24 @@
 using UnityEngine.UI;
 using Shared.SEUI;
 using TMPro;
+using ftUI;
 
 namespace Code.User_Interface.Zone {
   public class ZoneSettingTypeListItem : DynamicListItem<ZoneSettingType> {
     [Header("UI Elements")]
     [Tooltip("Label for the ZoneSettingType")]
     public TMP_Text label;
+    [Tooltip("Tooltip for the ComponentSettingType")]
+    public ftTooltip tooltip;
     [Tooltip("Toggle to select given ZoneSettingType")]
     public Toggle toggle;
 
     // ------------------------------------------------------------------------
     public override void SetItem(ZoneSettingType item) {
-      label.text = GetLabel(item);
+      var labelText = GetLabel(item);
+      name = labelText;
+      label.text = labelText;
+      tooltip.TooltipText = labelText;
     }
 
     // ------------------------------------------------------------------------
